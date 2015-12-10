8ce3771c005a272d9eb364cd890dd87ecd367bd3
Harry Gallagher <harrygallagher4@gmail.com>
2015-12-10 01:53:56 -0600

    Added EchoNest API functionality with sample
    Also added config.json.example. Copy this and insert your own API key

--- app.js
+++ app.js
@@ -2,4 +2,18 @@
  * This entire project uses ES2015 syntax. 
  * Run with `babel-node app` or `npm start`
  */
-console.log('TODO')
+import { api_key } from './config'
+import EchoNest from './lib/echonest'
+
+var nest = new EchoNest(api_key)
+
+nest.makeRequest('artist/search', {
+  name: 'schoolboy q',
+  bucket: 'id:spotify',
+  results: '1',
+  limit: true
+}, (error, response, body) => {
+  if (error)
+    return console.log(error)
+  return console.log(JSON.stringify(body, null, 2))
+})
